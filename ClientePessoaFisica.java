d7d0b27176a635d733a80d6ff02afed470278edf
RogerioR3 <117490600+RogerioR3@users.noreply.github.com>
2024-07-25 15:57:23 -0300

    Update ClientePessoaFisica.java
    up

--- ClientePessoaFisica.java
+++ ClientePessoaFisica.java
@@ -18,4 +18,45 @@ public class ClientePessoaFisica extends Cliente {
         if (this.cpf == cpf) return true;
         return false;
     }
+    
+    @Override
+    public void atualizaDados(Cliente cliente) {
+        if (cliente instanceof ClientePessoaFisica) {
+            ClientePessoaFisica pf = (ClientePessoaFisica) cliente;
+            this.setNome(pf.getNome());
+            this.setEmail(pf.getEmail());
+            this.setEndereco(pf.getEndereco());
+            this.setTelefone(pf.getTelefone());
+            this.setUserName(pf.getUserName());
+            this.setPassword(pf.getPassword());
+            this.setCpf(pf.getCpf());
+            this.setRg(pf.getRg());
+            this.setDataNascimento(pf.getDataNascimento());
+        }
+    }
+    
+    // Getters and Setters
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
