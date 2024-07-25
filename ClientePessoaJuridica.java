ca9a8d48bffa24a83ad26e97ec105fbd11785c0f
RogerioR3 <117490600+RogerioR3@users.noreply.github.com>
2024-07-25 16:01:05 -0300

    Update ClientePessoaJuridica.java
    up

--- ClientePessoaJuridica.java
+++ ClientePessoaJuridica.java
@@ -13,7 +13,49 @@ public class ClientePessoaJuridica extends Cliente {
     }
     
     public boolean validarCNPJ(String cnpj) {
+        //return this.cnpj.equals(cnpj);
         if (this.cnpj == cnpj) return true;
         return false;
+    }
+    
+    @Override
+    public void atualizaDados(Cliente cliente) {
+        if (cliente instanceof ClientePessoaJuridica) {
+            ClientePessoaJuridica pj = (ClientePessoaJuridica) cliente;
+            this.setNome(pj.getNome());
+            this.setEmail(pj.getEmail());
+            this.setEndereco(pj.getEndereco());
+            this.setTelefone(pj.getTelefone());
+            this.setUserName(pj.getUserName());
+            this.setPassword(pj.getPassword());
+            this.setCnpj(pj.getCnpj());
+            this.setRazaoSocial(pj.getRazaoSocial());
+            this.setInscricaoEstadual(pj.getInscricaoEstadual());
+        }
+    }
+    
+    // Getters and Setters
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
     }
 }
