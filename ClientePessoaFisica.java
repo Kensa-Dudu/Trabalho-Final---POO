2bbb2a3d7fddc7a6ab78950214d8757b5eeccb36
RogerioR3 <117490600+RogerioR3@users.noreply.github.com>
2024-07-25 14:57:08 -0300

    Update and rename ClientePessoaFísica to ClientePessoaFisica.java
    Esqueleto de ClientePessoaFisica.java

---
+++ ClientePessoaFisica.java
@@ -0,0 +1,18 @@
+public class ClientePessoaFisica extends Cliente {
+    private String cpf;
+    private String rg;
+    private Date dataNascimento;
+    
+    
+    public ClientePessoaFisica(int idCliente, String nome, String email, String endereco, String telefone, String userName, String password, String cpf, String rg, Date dataNascimento) {
+        super(idCliente, nome, email, endereco, telefone, userName, password);
+        this.cpf = cpf;
+        this.rg = rg;
+        this.dataNascimento = dataNascimento;
+        
+    }
+    
+    public boolean validarCPF(String cpf) {
+        
+    }
+}
