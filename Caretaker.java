ec320ac2c95ae686e3e62ea38d1b81adfbf9ae4a
RogerioR3 <117490600+RogerioR3@users.noreply.github.com>
2024-07-25 15:41:12 -0300

    Update Caretaker.java
    Esqueleto da classe Caretaker.java

--- Caretaker.java
+++ Caretaker.java
@@ -1,1 +1,16 @@
+import java.util.Stack;
 
+public class Caretaker {
+    private Stack<Memento> mementoStack = new Stack<>();
+
+    public void saveState(Pedido pedido) {
+        mementoStack.push(new Memento(pedido));
+    }
+
+    public Memento restoreState() {
+        if (!mementoStack.isEmpty()) {
+            return mementoStack.pop();
+        }
+        return null;
+    }
+}
