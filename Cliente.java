1221a5719fda8a8c7eabb99902a14a434a18e074
RogerioR3 <117490600+RogerioR3@users.noreply.github.com>
2024-07-29 15:31:24 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public abstract class Cliente {
+    protected Scanner sc = new Scanner(System.in);
+    
     private int idCliente;
     private String nome;
     private String email;
@@ -17,9 +24,66 @@ public abstract class Cliente {
         this.userName = userName;
         this.password = password;
     }
+    
+    public Cliente(int idCliente) {
+        int m = 0;
+        String senha = " ", confSenha = " ", email;
+        System.out.println("Insira os dados:");
+        
+        while(m < 7){
+            switch (m) {
+                case 0:
+                    System.out.print("  -> Nome = ");
+                    this.nome = sc.nextLine();
+                    m++;
+                    break;
+                case 1:
+                    System.out.print("  -> Email = ");
+                    email = sc.nextLine();
+                    if(isValidEmail(email)){
+                        this.email = email;
+                        m++;
+                    }else{
+                        System.out.println("Email inválido!");
+                    }
+                    break;
+                case 2:
+                    System.out.print("  -> Endereço = ");
+                    this.endereco = sc.nextLine();
+                    m++;
+                    break;
+                case 3:
+                    System.out.print("  -> Telefone = ");
+                    this.telefone = sc.nextLine();
+                    m++;
+                    break;
+                case 4:
+                    System.out.print("  -> Nome de Usuário = ");
+                    this.userName = sc.nextLine();
+                    m++;
+                    break;
+                case 5:
+                    System.out.print("  -> Senha = ");
+                    senha = sc.nextLine();
+                    m++;
+                    break;
+                case 6:
+                    System.out.print("  -> Confirmar Senha = ");
+                    confSenha = sc.nextLine();
+                    if(Objects.equals(senha, confSenha)){
+                        this.password = senha;
+                        m++;
+                    }else{
+                        System.out.println("As duas senhas devem ser iguais!");
+                        m--;
+                    }
+                    break;
+            }
+        }
+    }
 
     public boolean login(String userName, String password){
-        return true;
+        return this.userName.equals(userName) && this.password.equals(password);
     }
     
     public void atualizaDados(Cliente cliente){
@@ -89,6 +153,21 @@ public abstract class Cliente {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    //Verificação de email
+    private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+    public static boolean isValidEmail(String email) {
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    //isValidEmail(email)
+    
+    
 
 }
 
